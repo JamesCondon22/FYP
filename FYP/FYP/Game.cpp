ade3dae5f4c33c0af67870ea2af9d8e7d7af968b
James Condon <c00207200@itcarlow.ie>
2018-11-08 10:12:24 +0000

    Starter Kit Complete

---
+++ FYP/FYP/Game.cpp
@@ -0,0 +1,92 @@
+#include "Game.h"
+
+Game::Game()
+{
+	m_window = SDL_CreateWindow("Entity Component Systems", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1200, 700, SDL_WINDOW_OPENGL);
+	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+
+	if (IMG_Init(imgFlags) != imgFlags)
+	{
+		cout << "Error: " << IMG_GetError() << endl;
+	}
+
+
+	initialise();
+}
+
+Game::~Game()
+{
+}
+
+void Game::run()
+{
+	const Uint32 fps = 60;
+	const Uint32 minimumFrameTime = 1000 / fps;
+
+	Uint32 frameTime;
+	Uint32 lastFrameTime = 0;
+	Uint32 deltaTime = 0;
+
+	while (!exit) {
+
+		processEvents();
+		frameTime = SDL_GetTicks();
+
+		deltaTime = frameTime - lastFrameTime;
+		lastFrameTime = frameTime;
+
+		update();
+		render();
+
+		if ((SDL_GetTicks() - frameTime) < minimumFrameTime)
+			SDL_Delay(minimumFrameTime - (SDL_GetTicks() - frameTime));
+	}
+
+	SDL_DestroyRenderer(m_renderer);
+	SDL_DestroyWindow(m_window);
+	SDL_Quit();
+}
+
+void Game::processEvents()
+{
+
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+			exit = true;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				exit = true;
+			break;
+		}
+	}
+}
+
+void Game::update()
+{
+
+}
+
+void Game::render()
+{
+	if (m_renderer == nullptr)
+	{
+		SDL_Log("Could not create a renderer: %s", SDL_GetError());
+	}
+
+	SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
+	SDL_RenderClear(m_renderer);
+	SDL_RenderPresent(m_renderer);
+
+}
+
+
+void Game::initialise()
+{
+
+}
+
---
+++ FYP/FYP/Game.h
@@ -0,0 +1,39 @@
+#ifndef GAME
+#define GAME
+
+#include <SDL.h>
+#include <SDL_timer.h>
+#include <SDL_image.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+using namespace std;
+
+class Game {
+
+public:
+	Game();
+	~Game();
+
+	void run();
+
+private:
+
+	void processEvents();
+	void update();
+	void render();
+	void initialise();
+
+	SDL_Window *m_window;
+	SDL_Renderer *m_renderer;
+	SDL_Event event;
+	bool exit;
+
+	
+
+};
+
+
+
+#endif // !GAME
